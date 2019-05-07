a8dcaac16b97b3cb412a857790f1b3c99b70d543
pitardeveloper <37588350+pitardeveloper@users.noreply.github.com>
2019-05-08 01:24:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -319,6 +319,32 @@ client.on('message', msg => {
 
 });
 
+
+client.on('ready',async () => {
+  console.log(client.user.username);
+  try {
+     const config = {
+      name: "! Empire - Rainbow", // اسم الرتبة
+      guildid: "564528325914001419", // اي دي السيرفر
+      sec: 0.5 // عدد الثواني
+    };
+    let guild = client.guilds.get(config.guildid);
+    let role = guild.roles.find(role => role.name === config.name);
+    let sec = config.sec * 1000;
+    if(!guild) return console.warn("Unkown guild.");
+    if(!role) return console.warn("Unkown role");
+    if(role.position >= guild.members.get(client.user.id).highestRole.position) return console.warn("**يجب ان تكون رتبة البوت فوق رتبة الرينبو**");
+    setInterval(() => {
+      role.edit({ 
+      color: "RANDOM"
+    });
+    }, sec);
+  } catch(e) {
+    console.error(e);
+  }
+}); 
+
+
 client.on('message', msg => {
   //Code By : ‡ ♪ ℬℐℓѦℓ✋ ‡#2026
   if(msg.content.startsWith('/suggest')) {
@@ -355,32 +381,6 @@ client.on('message', message => {
       "\n" + "" + args + "")
   }
   });
-
-
-client.on('ready',async () => {
-  console.log(client.user.username);
-  try {
-     const config = {
-      name: "! Empire - Rainbow", // اسم الرتبة
-      guildid: "564528325914001419", // اي دي السيرفر
-      sec: 1 // عدد الثواني
-    };
-    let guild = client.guilds.get(config.guildid);
-    let role = guild.roles.find(role => role.name === config.name);
-    let sec = config.sec * 1000;
-    if(!guild) return console.warn("Unkown guild.");
-    if(!role) return console.warn("Unkown role");
-    if(role.position >= guild.members.get(client.user.id).highestRole.position) return console.warn("**يجب ان تكون رتبة البوت فوق رتبة الرينبو**");
-    setInterval(() => {
-      role.edit({ 
-      color: "RANDOM"
-    });
-    }, sec);
-  } catch(e) {
-    console.error(e);
-  }
-}); 
-
 
 
 // THIS  MUST  BE  THIS  WAY
